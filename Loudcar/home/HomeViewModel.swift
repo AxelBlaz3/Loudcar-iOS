9a2e24b20c227a9e6936504652d2cb8d0e374202
AxelBlaz3 <karthikgaddam4@gmail.com>
2021-04-30 11:21:39 +0530

    Abstract bluetooth implemention with Delegate
    Signed-off-by: AxelBlaz3 <karthikgaddam4@gmail.com>

--- Loudcar/home/HomeViewModel.swift
+++ Loudcar/home/HomeViewModel.swift
@@ -8,13 +8,21 @@
 import Foundation
 import CoreBluetooth
 
-class HomeViewModel: NSObject, ObservableObject, CBCentralManagerDelegate {
+class HomeViewModel: NSObject, ObservableObject {
     @Published var peripherals: [CBPeripheral] = []
     var centralManager: CBCentralManager!
     
-    func scanLeDevices() {
-        
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
+    
+    func scanPeripherals() {
+        centralManager.scanForPeripherals(withServices: nil, options: nil)
+    }
+}
+
+extension HomeViewModel: CBCentralManagerDelegate {
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         // Reject if the signal strength is too low to attempt data transfer.
@@ -38,6 +46,46 @@ class HomeViewModel: NSObject, ObservableObject, CBCentralManagerDelegate {
     }
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        
+        switch central.state {
+        case .poweredOn:
+            // ... so start working with the peripheral
+            print("CBManager is powered on")
+            //retrievePeripheral()
+        case .poweredOff:
+            print("CBManager is not powered on")
+            // In a real app, you'd deal with all the states accordingly
+            return
+        case .resetting:
+            print("CBManager is resetting")
+            // In a real app, you'd deal with all the states accordingly
+            return
+        case .unauthorized:
+            // In a real app, you'd deal with all the states accordingly
+            if #available(iOS 13.0, *) {
+                switch central.authorization {
+                case .denied:
+                    print("You are not authorized to use Bluetooth")
+                case .restricted:
+                    print("Bluetooth is restricted")
+                default:
+                    print("Unexpected authorization")
+                }
+            } else {
+                // Fallback on earlier versions
+            }
+            return
+        case .unknown:
+            print("CBManager state is unknown")
+            // In a real app, you'd deal with all the states accordingly
+            return
+        case .unsupported:
+            print("Bluetooth is not supported on this device")
+            // In a real app, you'd deal with all the states accordingly
+            return
+        @unknown default:
+            print("A previously unknown central manager state occurred")
+            // In a real app, you'd deal with yet unknown cases that might occur in the future
+            return
+        }
     }
 }
